146bde4307e3380c6e50e51f0c1f14c37de4070a
CTCycle <thomas.virdis89@gmail.com>
2024-07-13 18:16:50 +0200

    moved configurations.json into settings

--- XREPORT/commons/constants.py
+++ XREPORT/commons/constants.py
@@ -12,6 +12,6 @@ GENERATION_INPUT_PATH = join(RSC_PATH, 'generation', 'input_images')
 GENERATION_OUTPUT_PATH = join(RSC_PATH, 'generation', 'reports')
 LOGS_PATH = join(PROJECT_DIR, 'resources', 'logs')
 
-CONFIG_PATH = join(PROJECT_DIR, 'configurations.json')
+CONFIG_PATH = join(PROJECT_DIR, 'settings', 'configurations.json')
 with open(CONFIG_PATH, 'r') as file:
     CONFIG = json.load(file)
