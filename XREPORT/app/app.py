0d7cc03725161ff1d1a627c0ed2f074809ddedec
CTCycle <thomas.virdis89@gmail.com>
2025-08-05 12:32:35 +0200

    switched to qt materials style

--- XREPORT/app/app.py
+++ XREPORT/app/app.py
@@ -1,5 +1,6 @@
 import sys
 from PySide6.QtWidgets import QApplication
+from qt_material import apply_stylesheet
 
 # [SETTING WARNINGS]
 import warnings
@@ -7,7 +8,7 @@ warnings.simplefilter(action='ignore', category=Warning)
 
 # [IMPORT CUSTOM MODULES]
 from XREPORT.app.interface.window import MainWindow
-from XREPORT.app.constants import UI_PATH, QSS_PATH
+from XREPORT.app.constants import UI_PATH
 
 
 # [RUN MAIN]
@@ -15,9 +16,9 @@ from XREPORT.app.constants import UI_PATH, QSS_PATH
 if __name__ == "__main__":  
     app = QApplication(sys.argv) 
 
-    # ---- LOAD QSS STYLE ----
-    with open(QSS_PATH, "r") as f:
-        app.setStyleSheet(f.read())
+    # setup stylesheet
+    extra = {'density_scale': '-1'}
+    apply_stylesheet(app, theme='dark_teal.xml', extra=extra)
 
     main_window = MainWindow(UI_PATH)   
     main_window.show()
