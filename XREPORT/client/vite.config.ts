cb08a6e75249f70b6cd70de6fb5db2d2e968460c
CTCycle <thomas.virdis89@gmail.com>
2025-12-17 19:49:47 +0100

    add InferencePage component with image upload and report generation functionality

--- XREPORT/client/vite.config.ts
+++ XREPORT/client/vite.config.ts
@@ -4,4 +4,16 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
     plugins: [react()],
+    server: {
+        proxy: {
+            '/base': {
+                target: 'http://127.0.0.1:8000',
+                changeOrigin: true,
+            },
+            '/data': {
+                target: 'http://127.0.0.1:8000',
+                changeOrigin: true,
+            },
+        },
+    },
 })
