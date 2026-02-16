29faa46370f0eed696b5a7a7cf9e30d65753761d
CTCycle <thomas.virdis89@gmail.com>
2026-02-16 09:12:40 +0100

    update inference job polling mechanism and clean up unused code

--- XREPORT/server/entities/inference.py
+++ XREPORT/server/entities/inference.py
@@ -25,16 +25,3 @@ class CheckpointsResponse(BaseModel):
     checkpoints: list[CheckpointInfo]
     success: bool
     message: str
-
-
-###############################################################################
-class GenerationRequest(BaseModel):
-    checkpoint: str
-    generation_mode: str
-
-
-###############################################################################
-class GenerationResponse(BaseModel):
-    success: bool
-    message: str
-    reports: dict[str, str] | None = None
